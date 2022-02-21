3697f9949569e2deb24165ae72f2a61bdd9256d8
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-02-21 16:17:42 +0200

    docs: remove persist and add fetch

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,42 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contacts/contactsReducer";
-
-const contactsPersistConfig = {
-  key: "items",
-  version: 1,
-  storage,
-  blacklist: ["filter"],
-};
-
-const contactsPersistedReducer = persistReducer(
-  contactsPersistConfig,
-  contactsReducer
-);
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: {
-    contacts: contactsPersistedReducer,
+    contacts: contactsReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  logger,
   // devTools: process.env.NODE_ENV === "development",
 });
-
-export const persistor = persistStore(store);
 
 export default store;
