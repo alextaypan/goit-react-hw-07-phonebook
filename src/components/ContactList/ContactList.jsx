feb49251326847f872e8f5c76404973e44cb6cc0
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-02-21 21:41:51 +0200

    add gh-pages

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -52,7 +52,7 @@ ContactList.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
+      phone: PropTypes.string.isRequired,
     })
   ),
 };
